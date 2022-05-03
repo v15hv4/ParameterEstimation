3ade3cb0c12d31f85e5bff953d41b8582420192c
v15hv4 <vishva2912@gmail.com>
2022-05-03 19:02:46 +0530

    Add noisy output data

--- generate_data_dsl.jl
+++ generate_data_dsl.jl
@@ -1,4 +1,4 @@
-using CSV, Catalyst, Plots, DataFrames, DifferentialEquations
+using CSV, Catalyst, Plots, DataFrames, DifferentialEquations, Random, Distributions
 
 # config
 MEASUREMENTS_PATH = "measurements.csv"
@@ -14,6 +14,7 @@ end α_1 α_2 β_0 β_1 β_2
 
 # timespan of experiment
 tspan = (0.0, 50.0)
+teval = (tspan[2] - tspan[1]) / 1000
 
 # nominal parameters
 p = (
@@ -35,19 +36,25 @@ u0 = [
 problem = ODEProblem(rn, u0, tspan, p)
 
 # solve the ODE
-sol = solve(problem, Tsit5(), saveat=1.0)
-sol_X = sol[1,:]
-sol_Y = sol[2,:]
+sol = solve(problem, Tsit5(), saveat=teval)
+sol_X = sol[1, :]
+sol_Y = sol[2, :]
+
+# add noise to observations
+add_noise = rand(Normal(0.0, 0.05), size(sol_X))
+mul_noise = rand(LogNormal(0.0, 0.05), size(sol_X))
+output = (sol_X .* mul_noise) .+ add_noise
 
 # plot solution (& save plot)
-plot(sol.t, sol_X, label="X(t)", seriestype=:scatter)
-plot!(sol.t, sol_Y, label="Y(t)", seriestype=:scatter)
+plot(sol.t, output, color="black", alpha=0.3, label="output", seriestype=:scatter)
+plot!(sol.t, sol_X, color="blue", label="X(t)")
+plot!(sol.t, sol_Y, color="red", label="Y(t)")
 xlabel!("t")
 ylabel!("activity")
 savefig("sol.png")
 
 # construct dataframe
-df = DataFrame(time=sol.t, X=sol_X, Y=sol_Y)
+df = DataFrame(time=sol.t, X=sol_X, Y=sol_Y, output=output)
 println(df)
 
 # save as csv
